9f0c1a9d4cb537eaf3709399da7bb4bc77908572
Amal Sony <amalsony@Amals-MacBook-Pro-2.local>
2022-10-30 09:18:13 -0400

    addedformlink

--- components/WaitlistButton.jsx
+++ components/WaitlistButton.jsx
@@ -1,8 +1,14 @@
 import classes from "./WaitlistButton.module.css";
+import Script from "next/script";
 
 export default function WaitlistButton({ type }) {
   return (
-    <a className={type === "gold" ? classes.gold : classes.button}>
+    <a
+      className={type === "gold" ? classes.gold : classes.button}
+      target="_blank"
+      rel="noreferrer"
+      href="https://oay0uma7h4g.typeform.com/to/Wubk9wwD"
+    >
       <span className={classes.text}>Join the waitlist</span>
     </a>
   );
